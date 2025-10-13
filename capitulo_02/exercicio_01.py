83c8823b648668dc737c699981912f7a02ff9aa7
Iury Oliveira <iury.oliveira.dev@gmail.com>
2025-10-13 18:26:35 -0300

    feat: adiciona exercício 01 do capítulo 2

---
+++ capitulo_02/exercicio_01.py
@@ -0,0 +1,10 @@
+# Exercício 2.1 - Conversão de expressões matemáticas para Python
+
+# Expressão: 10 + 20 × 30
+print(10 + 20 * 30) # Resultado: 610
+
+# Expressão: 4² ÷ 30
+print(4 ** 2 / 30) # Resultado: 0.533...
+
+# Expressão: (9⁴ + 2) × 6 - 1
+print((9 ** 4 + 2)* 6 - 1) # Resultado: 39365

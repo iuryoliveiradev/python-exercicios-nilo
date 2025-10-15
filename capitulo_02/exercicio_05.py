a9b9782e3f053538d1267efd366d039ff4873887
Iury Oliveira <iury.oliveira.dev@gmail.com>
2025-10-15 20:17:51 -0300

    feat: adiciona exercício 05 do capítulo 02 - calcula soma de três variáveis

---
+++ capitulo_02/exercicio_05.py
@@ -0,0 +1,13 @@
+#Somar 3 Variaveis distintas e exibir o resultado
+
+# Os valores escolidos são: 15, 10 e 25
+
+#Definindo as variaveis
+
+variavel_01 = 15
+variavel_02 = 10
+variavel_03 = 25
+resultado_soma = variavel_01 + variavel_02 +variavel_03
+
+#Exibindo a soma
+print(f"O resultado de {variavel_01} + {variavel_02} + {variavel_03} é: {resultado_soma}!") #Resultado 50

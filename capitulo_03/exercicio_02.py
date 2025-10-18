657c422476179e47e52ca69802681611459ed043
Iury Oliveira <iury.oliveira.dev@gmail.com>
2025-10-18 19:33:00 -0300

    feat: adiciona exercício 02 do capítulo 3 - verifica aprovação com base nas notas individuais

---
+++ capitulo_03/exercicio_02.py
@@ -0,0 +1,12 @@
+# Exercício 3.2 - Verifica se o aluno foi aprovado com base nas notas individuais
+
+# Notas do aluno em cada matéria
+materia1 = 9
+materia2 = 5
+materia3 = 10
+
+# Verificando se todas as notas são maiores ou iguais a 7
+if materia1 >= 7 and materia2 >= 7 and materia3 >= 7:
+    print("O aluno foi aprovado")
+else:
+    print("O aluno foi reprovado")

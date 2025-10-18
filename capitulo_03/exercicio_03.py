b1a44c1fa5d32f610a7bf45776abfabdb8072100
Iury Oliveira <iury.oliveira.dev@gmail.com>
2025-10-18 19:53:47 -0300

    feat: adiciona exercício 03 do capítulo 3 - soma dois números inteiros informados pelo usuário

---
+++ capitulo_03/exercicio_03.py
@@ -0,0 +1,11 @@
+# Exercício 3.3 - Exibe a soma de 2 números inteiros informados
+
+# Solicitando os números
+valor_01 = int(input("Digite um número inteiro: "))
+valor_02 = int(input("Digite outro número inteiro: "))
+
+# Calculando a soma
+soma = valor_01 + valor_02
+
+# Exibindo o resultado
+print(f"A soma de {valor_01} + {valor_02} é igual a {soma}")

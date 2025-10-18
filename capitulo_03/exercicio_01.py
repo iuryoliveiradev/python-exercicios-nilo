163fb9b73e6b4d0ccc449fdf78c442a414ff16c2
Iury Oliveira <iury.oliveira.dev@gmail.com>
2025-10-17 21:34:40 -0300

    feat: adiciona exercício 01 do capítulo 3 - verifica se a pessoa deve pagar imposto com base no salário

---
+++ capitulo_03/exercicio_01.py
@@ -0,0 +1,16 @@
+# Exercício 3.1 - Determina se a pessoa deve pagar imposto ou não
+
+# Definindo salário de 2 pessoas fictícias (Ana e Brenda)
+salario_ana = 1200.00
+salario_brenda = 1350.00
+
+# Para pagar imposto o salário deve ser maior que R$ 1.200,00
+if salario_ana > 1200:
+    print("Ana terá que pagar imposto!")
+else:
+    print("Ana não pagará imposto!")
+
+if salario_brenda > 1200:
+    print("Brenda terá que pagar imposto!")
+else:
+    print("Brenda não pagará imposto!")

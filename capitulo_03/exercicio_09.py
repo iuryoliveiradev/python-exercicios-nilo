291733aec1cd162fbd1ba36519a9df536ce14a08
Iury Oliveira <iury.oliveira.dev@gmail.com>
2025-10-21 20:28:12 -0300

    feat: adiciona exercício 09 do cápitulo 3 - converte temperatura de Celsius para Fahrenheit

---
+++ capitulo_03/exercicio_09.py
@@ -0,0 +1,10 @@
+# Exercício 3.9 - Conversor de °C para °F
+
+# Solicitando informação
+celsius = float(input("Qual a temperatura em °C? "))
+
+# Convertendo para Fahrenheit
+fahrenheit = 9 * celsius / 5 + 32
+
+# Mensagem de conversão
+print(f"{celsius:.1f}°C equivalem a {fahrenheit:.1f}°F") 

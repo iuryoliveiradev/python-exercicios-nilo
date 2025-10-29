fecb222d3283d7c04a5d4d15fff3d9b61695160e
Iury Oliveira <iury.oliveira.dev@gmail.com>
2025-10-29 20:55:08 -0300

    fix: corrige erro de digitação e melhora comentários no exercício 4.5

--- capitulo_04/exercicio_05.py
+++ capitulo_04/exercicio_05.py
@@ -1,16 +1,16 @@
 # Exercício 4.5 - Calcula o preço da passagem de acordo com a distância
 
-# Coletando informações para calculo
-distancia = int(input("Qual a distância que deseja percorer em km? "))
+# Entrada de dados
+distancia = int(input("Qual a distância que deseja percorrer em km? "))
 
-# Definindo preço por km total
+# Definindo valor por km conforme distância
 if distancia <= 200:
     valor = 0.50
 else:
     valor = 0.45
 
-# Descobrindo valor da passagem para cobrança
+# Calculando valor total da passagem
 valor_passagem = distancia * valor
 
-# Exibindo informação ao passageiro
+# Exibindo valor final da passagem ao passageiro
 print(f"O valor da sua passagem é de R${valor_passagem:.2f}")
